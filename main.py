35635c6aadbb3f66e605eed6d5d56440fd4a0935
João Henrique <joaowashere@hotmail.com>
2020-12-25 19:44:24 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+var1 = float( input('Primeiro Número: ') )
+var2 = float( input('Segundo Número: ') )
+
+somar = var1 + var2
+
+subtrair = var1 - var2
+
+multiplicar = var1 * var2
+
+divisao = var1 / var2
+
+exponenciacao = var1 ** var2
+
+restodivisao = var1 % var2
+
+# Resultados:
+print('Soma:             ', var1,'+',var2,' = ' , somar)
+print('Subtração:        ', var1,'-',var2,' = ' , subtrair)
+print('Multiplicação:    ', var1,'*',var2,' = ' , multiplicar)
+print('Divisão:          ', var1,'/',var2,' = ' , divisao)
+print('Exponenciação:    ', var1,'**',var2,' = ', exponenciacao)
+print('Resto da Divisão: ', var1,'%',var2,' = ' , restodivisao)
